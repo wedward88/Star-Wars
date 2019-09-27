c57816630931a488971b8fe2c0a01e176d6588b6
wdunn <williamdunn195@gmail.com>
2019-09-27 13:46:44 -0400

    create production build

---
+++ docs/precache-manifest.80121b40520f658ee2bd73f2fd66ef92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7778ad5f5548af7f40e2ff1248762328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56234ce41398cab35b97",
+    "url": "/static/css/main.f267a391.chunk.css"
+  },
+  {
+    "revision": "b3791433c0d04d9735fe",
+    "url": "/static/js/2.cfbb8aed.chunk.js"
+  },
+  {
+    "revision": "56234ce41398cab35b97",
+    "url": "/static/js/main.777e4626.chunk.js"
+  },
+  {
+    "revision": "5a475f47354cb087e95f",
+    "url": "/static/js/runtime-main.5b800f18.js"
+  },
+  {
+    "revision": "c683e91df41251b79c6cfbb2f61dde58",
+    "url": "/static/media/starjedi-webfont.c683e91d.woff2"
+  },
+  {
+    "revision": "d6e1163f553754dc301ab87b39407fc3",
+    "url": "/static/media/starjedi-webfont.d6e1163f.woff"
+  }
+]);
