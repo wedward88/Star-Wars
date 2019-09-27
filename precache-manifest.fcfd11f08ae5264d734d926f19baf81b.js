0b73708a9b94501e74e394f26ff7f31a59941750
wdunn <williamdunn195@gmail.com>
2019-09-27 14:24:57 -0400

    Updates

---
+++ precache-manifest.fcfd11f08ae5264d734d926f19baf81b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe9a82ce2bd105c0ff54052595f342f",
+    "url": "/Star-Wars/index.html"
+  },
+  {
+    "revision": "fe82265b8de16348bf92",
+    "url": "/Star-Wars/static/css/main.700fadad.chunk.css"
+  },
+  {
+    "revision": "b3791433c0d04d9735fe",
+    "url": "/Star-Wars/static/js/2.cfbb8aed.chunk.js"
+  },
+  {
+    "revision": "fe82265b8de16348bf92",
+    "url": "/Star-Wars/static/js/main.777e4626.chunk.js"
+  },
+  {
+    "revision": "d105469428ef883ee58e",
+    "url": "/Star-Wars/static/js/runtime-main.37c7c101.js"
+  },
+  {
+    "revision": "c683e91df41251b79c6cfbb2f61dde58",
+    "url": "/Star-Wars/static/media/starjedi-webfont.c683e91d.woff2"
+  },
+  {
+    "revision": "d6e1163f553754dc301ab87b39407fc3",
+    "url": "/Star-Wars/static/media/starjedi-webfont.d6e1163f.woff"
+  }
+]);
